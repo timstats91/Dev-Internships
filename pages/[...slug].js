19d281487bb0ca63e628eb8853090ffdb3babd17
timstats91 <timstats91@gmail.com>
2023-01-07 09:34:23 -0600

    filter homepage fix

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -32,7 +32,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths: [...data.pages.nodes, ...data.properties.nodes, ...data.posts.nodes]
-            .filter((page) => page.uri === "/")
+            .filter((page) => page.uri !== "/")
             .map(page => ({
                 params: {
                     slug: page.uri.substring(1, page.uri.length - 1).split("/")
