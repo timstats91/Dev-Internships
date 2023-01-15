7ccfde16e4795a490fc7dae6c4f2127cf3e9e860
timstats91 <timstats91@gmail.com>
2023-01-15 12:32:29 -0600

    fix gap on pagination

--- components/BlogPosts/Pagination/Pagination.js
+++ components/BlogPosts/Pagination/Pagination.js
@@ -1,6 +1,6 @@
 export const Pagination = ({ totalPages, onPageClick }) => {
     return (
-        <div className="max-w-5xl mx-auto my-11 flex gap-2 justify-center">
+        <div className="max-w-5xl mx-auto my-11 flex justify-center">
             {Array.from({length: totalPages}).map((_, i) => (
                 <div key={i} className="btn-page" onClick={() => {
                     onPageClick(i + 1)
