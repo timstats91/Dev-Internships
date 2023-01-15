da1ca9050b72c5475b12c85bd7b41652d15faba6
timstats91 <timstats91@gmail.com>
2023-01-15 12:21:21 -0600

    update footer and nav

--- components/MainMenu/MainMenu.js
+++ components/MainMenu/MainMenu.js
@@ -7,14 +7,14 @@ export const MainMenu = ({items}) => {
     const [showNav, setShowNav] = useState(false);
 
     return (
-        <nav className="bg-slate-800 text-white md:px-5 h-[64px] justify-between sticky top-0 z-20 flex">
+        <nav className="bg-slate-800 text-white md:px-5 h-[64px] justify-between sticky top-0 z-50 flex">
             <Link href="/">
                 <div className="py-4 px-5 flex cursor-pointer hover:bg-slate-700">
                     <FaCode size={30} className="text-purple-500"/>
                     <p className="text-2xl font-semibold pl-2">Dev Internships</p>
                 </div>
             </Link>
-            <div className={(showNav ? "top-16" : "-top-full") + " md:flex md:flex-1 md:justify-end fixed md:static mx-auto text-center bg-slate-800 w-screen"}>
+            <div className={(showNav ? "top-16" : "-top-full") + " md:flex md:flex-1 md:justify-end fixed md:static mx-auto text-center bg-slate-800 w-screen z-50"}>
                 {/* <button className="block justify-end md:hidden focus:outline-none p-5 hover:bg-slate-700">
                     <FaTimes size={25} />
                 </button> */}
